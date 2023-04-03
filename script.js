7238aea5257ebc007046c02690f8cd459bb9615e
James <94071851+HiBad07@users.noreply.github.com>
2023-04-03 16:34:18 +0100

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ searchBtn.addEventListener('click', () => {
   const city = searchBar.value;
   const apiKey = 'b445d38a8607d15f8d8afac046330276';
   const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
-  
+
   fetch(apiUrl)
     .then(response => response.json())
     .then(data => {
@@ -17,4 +17,15 @@ searchBtn.addEventListener('click', () => {
       const temp = Math.round(data.main.temp);
       const desc = data.weather[0].description;
       const iconCode = data.weather[0].icon;
-      const iconUrl
+      const iconUrl = `http://openweathermap.org/img/w/${iconCode}.png`;
+
+      locationDiv.textContent = location;
+      tempDiv.textContent = temp + '°C';
+      descDiv.textContent = desc;
+      iconDiv.innerHTML = `<img src="${iconUrl}" alt="${desc}">`;
+    })
+    .catch(error => {
+      console.log(error);
+      alert('City not found! Please try again.');
+    });
+});
