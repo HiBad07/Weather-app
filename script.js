ae6f0d3c8f8ba82b3f67670e0c77baa37bbbc228
James <94071851+HiBad07@users.noreply.github.com>
2023-04-03 16:38:06 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,19 @@
-const searchBar = document.querySelector('#search-bar');
-const searchBtn = document.querySelector('#search-btn');
-const locationDiv = document.querySelector('.location');
-const tempDiv = document.querySelector('.temperature');
-const descDiv = document.querySelector('.description');
-const iconDiv = document.querySelector('.icon');
+document.getElementById("get-weather").addEventListener("click", () => {
+  const city = document.getElementById("city-input").value;
+  
+  if (city) {
+    const apiKey = "b445d38a8607d15f8d8afac046330276";
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
-searchBtn.addEventListener('click', () => {
-  const city = searchBar.value;
-  const apiKey = 'b445d38a8607d15f8d8afac046330276';
-  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
-
-  fetch(apiUrl)
-    .then(response => response.json())
-    .then(data => {
-      const location = data.name + ', ' + data.sys.country;
-      const temp = Math.round(data.main.temp);
-      const desc = data.weather[0].description;
-      const iconCode = data.weather[0].icon;
-      const iconUrl = `http://openweathermap.org/img/w/${iconCode}.png`;
-
-      locationDiv.textContent = location;
-      tempDiv.textContent = temp + '°C';
-      descDiv.textContent = desc;
-      iconDiv.innerHTML = `<img src="${iconUrl}" alt="${desc}">`;
-    })
-    .catch(error => {
-      console.log(error);
-      alert('City not found! Please try again.');
-    });
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        const weatherInfo = `Weather in ${data.name}: ${data.weather[0].main} - ${data.main.temp}°C`;
+        document.getElementById("weather-info").innerText = weatherInfo;
+      })
+      .catch(error => {
+        console.error("Error fetching data:", error);
+        document.getElementById("weather-info").innerText = "City not found.";
+      });
+  }
 });
